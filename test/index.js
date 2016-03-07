49f24ac209c224b1c6ac030ac8efe6a4c4a5ecdc
leeight <leeight@gmail.com>
2016-03-07 11:03:26 +0800

    Fix test/index.js

--- test/index.js
+++ test/index.js
@@ -1,6 +1,36 @@
 'use strict';
 
+var fs = require('fs');
+var path = require('path');
+
 var p2pspider = require('../lib/index');
+
+var kDataDir = path.join(__dirname, '..', 'data');
+
+function dumpMeta(meta) {
+    var name = meta.info.name.toString();
+    var magnet = meta.magnet;
+    var files = [];
+    (meta.info.files || []).forEach(function (file) {
+        file.path.forEach(function (path) {
+            files.push(path.toString());
+        });
+    });
+
+    var infohash = meta.infohash;
+    var abs = path.join(kDataDir, infohash);
+    if (fs.existsSync(abs)) {
+        console.log('[*] %s - %s', magnet, name);
+        return;
+    }
+    console.log('[+] %s - %s', magnet, name);
+    fs.writeFileSync(abs, JSON.stringify({
+        name: name,
+        magnet: magnet,
+        files: files
+    }));
+}
+
 
 p2pspider(
     {
@@ -10,11 +40,14 @@ p2pspider(
         maxConnections: 400, // be careful
         timeout: 5000,
         filter: function(infohash, callback) {
-            var theInfohashIsExistsInDatabase = false;
-            callback(theInfohashIsExistsInDatabase);
+            var abs = path.join(kDataDir, infohash);
+            callback(fs.existsSync(abs));
+            // var theInfohashIsExistsInDatabase = false;
+            // callback(theInfohashIsExistsInDatabase);
         }
     },
     function(metadata) {
-        console.log(metadata);
+        dumpMeta(metadata);
+        // console.log(JSON.stringify(metadata, null, 2));
     }
 );
